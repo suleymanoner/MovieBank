7c1bb7e327c7c9297458b17ae39d9bae7223d563
Suleyman Oner <suleymanoner1999@gmail.com>
2023-08-01 00:40:03 +0200

    homescreen edited

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,34 +1,79 @@
 import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, FlatList} from 'react-native';
 import {connect} from 'react-redux';
-import {ApplicationState, MovieState, onGetMovies, onGetGenres} from '../redux';
+import {
+  ApplicationState,
+  MovieState,
+  onGetNowPlayingMovies,
+  onGetTopRatedMovies,
+  onGetPopularMovies,
+  onGetUpcomingMovies,
+  onGetGenres,
+  Movie,
+  MovieWithDates,
+} from '../redux';
 import {BACKGROUND_COLOR} from '../utils/Config';
-import MovieCardNew from '../components/MovieCardNew';
 import MovCard from '../components/MovCard';
 
 interface HomeScreenProps {
+  route: any;
   movieReducer: MovieState;
-  fetchMovies: Function;
-  fetchIndvMovie: Function;
+  fetchNowPlayingMovies: Function;
+  fetchTopRatedMovies: Function;
+  fetchPopularMovies: Function;
+  fetchUpcomingMovies: Function;
   fetchGenres: Function;
 }
 
 const _HomeScreen: React.FC<HomeScreenProps> = ({
+  route,
   navigation,
   movieReducer,
-  fetchMovies,
+  fetchNowPlayingMovies,
+  fetchTopRatedMovies,
+  fetchPopularMovies,
+  fetchUpcomingMovies,
   fetchGenres,
 }) => {
-  const {movies, genres} = movieReducer;
-  const [page, setPage] = useState<number>(1);
+  const {movie_list_type, pg} = route.params;
+  const {now_playing, top_rated, popular, upcoming, genres} = movieReducer;
+  const [page, setPage] = useState<number>(pg);
+  const [movies, setMovies] = useState<Movie[] | MovieWithDates>([]);
 
   useEffect(() => {
     fetchGenres();
   });
 
   useEffect(() => {
-    fetchMovies(page);
-  }, [page, fetchMovies]);
+    if (movie_list_type === 'now') {
+      if (page !== 1) {
+        fetchNowPlayingMovies(page);
+      }
+      setMovies(now_playing.movies);
+    } else if (movie_list_type === 'top') {
+      if (page !== 1) {
+        fetchTopRatedMovies(page);
+      }
+      setMovies(top_rated);
+    } else if (movie_list_type === 'pop') {
+      if (page !== 1) {
+        fetchPopularMovies(page);
+      }
+      setMovies(popular);
+    } else if (movie_list_type === 'up') {
+      if (page !== 1) {
+        fetchUpcomingMovies(page);
+      }
+      setMovies(upcoming.movies);
+    } else {
+      if (page !== 1) {
+        fetchNowPlayingMovies(page);
+      }
+      setMovies(now_playing.movies);
+    }
+    console.log('PAGE: ' + page);
+    console.log('TOTAL MOV: ' + now_playing.movies.length);
+  }, [page]);
 
   const goDetail = (id: number) => {
     navigation.navigate('Detail', {mov_id: id});
@@ -95,7 +140,10 @@ const mapToStateProps = (state: ApplicationState) => ({
 });
 
 const HomeScreen = connect(mapToStateProps, {
-  fetchMovies: onGetMovies,
+  fetchNowPlayingMovies: onGetNowPlayingMovies,
+  fetchTopRatedMovies: onGetTopRatedMovies,
+  fetchPopularMovies: onGetPopularMovies,
+  fetchUpcomingMovies: onGetUpcomingMovies,
   fetchGenres: onGetGenres,
 })(_HomeScreen);
 
